dc8be6f9b47a387d8e50cc5a736599c43d7e6d49
Meet Patel <meetpatel19aug@gmail.com>
2022-06-09 16:12:21 +0530

    Day9: <React> [Evening - Axios concept and API Call & initalization app for book store].

--- testapp/src/Components/Person.tsx
+++ testapp/src/Components/Person.tsx
@@ -9,6 +9,30 @@ type PersonProps = {
 export const Person: React.FC<PersonProps> = ({ firstName, lastName }) => {
   const UserData = useContext(userContext);
   console.log(UserData);
+
+  //Without Axios
+  // const GetData = async ()=>{
+  //   try {
+  //     const response = await fetch("http://apicalls/api",{
+  //     method:"POST",
+  //     headers:{
+  //       "Content-type": "application/json",
+  //     },
+  //     body:{}
+  //     });
+  //   } catch (error) {
+  //    console.log("Error Occured: "); 
+  //   }
+  // }
+  //With Axios with promise
+  // const GetData = ()=>{
+  //   axios.get("http://apicalls/asd/dsa",data, {
+  //       headers:{
+
+  //       }
+  //   }).then((re)=>console.log(re));
+  // }
+
   return (
     <>
       <h1>Person Component</h1>
